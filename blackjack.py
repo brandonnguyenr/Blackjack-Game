49e67313fd816c3c0fe33568c8d246d1da1106e7
brandonnguyenr <nguyen.brandon771@gmail.com>
2023-06-11 13:59:43 -0700

    tested out some functions and tested out game logic for game class

--- blackjack.py
+++ blackjack.py
@@ -5,19 +5,39 @@
 
 """ This is a simple Blackjack Game that is played against a dealer."""
 
+from blackjackgame import player
+from blackjackgame import cards
+
 def main():
     """ This function calls the beginning of the game and loops until the game ends. """
+    deck1 = cards.Deck()
+    total_betting_amount = 10000
+
     print('\nWelcome to Blackjack! \
            \nThis game is created by Brandon Nguyen \
            \nPlease contact me at nguyen.brandon771@csu.fullerton.edu for more information!')
     print('\nReady to Win Big? Place your bets now! \
            \nThis game is a 3:2 Payout! \
            \nHave Fun!')
-    name = input('\nEnter your name: ')
-    betting_amount = input('Hello ' + name + ', How much are you betting today?: $')
-
     
+    name = input('\nEnter your name: ')
+    betting_amount = input('Out of $10,000, how much would you like to bet?: $')
 
+    deck1.shuffle()
+    deck1.shuffle()
+    deck1.shuffle()
+
+    player1 = player.Player(name)
+    dealer  = player.Player('Dealer') 
+
+    card = deck1.deal()
+    player1.add_card(card)
+
+    card2 = deck1.deal()
+    dealer.add_card(card2) 
+    
+    print(player1)
+    print(dealer)
 
 if __name__ == '__main__':
     main()
