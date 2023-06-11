4efe873cc2b28f5501807ab0a4062b8eb6936842
brandonnguyenr <nguyen.brandon771@gmail.com>
2023-06-11 03:49:31 -0700

    added dialogue to the main function

--- blackjack.py
+++ blackjack.py
@@ -5,11 +5,19 @@
 
 """ This is a simple Blackjack Game that is played against a dealer."""
 
-from blackjackgame.game import start_game
-
 def main():
-    """ This function calls the blackjack game directory and loops until the game ends."""
-    start_game()
+    """ This function calls the beginning of the game and loops until the game ends. """
+    print('\nWelcome to Blackjack! \
+           \nThis game is created by Brandon Nguyen \
+           \nPlease contact me at nguyen.brandon771@csu.fullerton.edu for more information!')
+    print('\nReady to Win Big? Place your bets now! \
+           \nThis game is a 3:2 Payout! \
+           \nHave Fun!')
+    name = input('\nEnter your name: ')
+    betting_amount = input('Hello ' + name + ', How much are you betting today?: $')
+
+    
+
 
 if __name__ == '__main__':
     main()
